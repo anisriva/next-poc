8a895cbb94d24367ceeaf8a1838258d52ab4edaf
anisriva <postanisrivastava@outlook.com>
2024-11-04 20:56:58 +0530

    Applying fonts to next app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,14 @@ import type { Metadata } from "next";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./globals.css";
+import { Roboto} from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Nextjs POC",
   description: "What's next?🤨",
 };
+
+const font = Roboto({weight : '300', subsets : ['latin']});
 
 export default function RootLayout({
   children,
@@ -17,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark text-foreground bg-background">
       <head>{/* Any additional head content or SEO */}</head>
-      <body className={`antialiased`}>
+      <body className={`antialiased ${font.className}`}>
         <Providers>
           <Header />
           <main className="p-2 min-h-screen">{children}</main>
